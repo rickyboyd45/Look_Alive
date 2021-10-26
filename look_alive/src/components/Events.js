8c229b97f271ca90822af5d362698c16fc566ef4
Ricky Boyd <rickyboyd23@gmail.com>
2021-10-26 13:01:06 -0400

    update

--- look_alive/src/components/Events.js
+++ look_alive/src/components/Events.js
@@ -1,12 +1,18 @@
+import React from "react";
 
-
-const Events = () => {
-
-  return (
-    <div>
-
+const Events = ({ event, date, venue, ticketInfo, imageURL }) => (
+  <div className="card">
+    <img className="card-img-top" src={imageURL[0].url} alt="Event Poster" />
+    <div className="card-body">
+      <h5 className="card-title">{event}</h5>
+      <p className="card-text">{venue}</p>
+      <p className="card-text">{ticketInfo}</p>
+      <p className="card-text">
+      <small className="text-muted">{date}</small>
+      </p>
     </div>
-  )
-} 
+  </div>
+);
+
 
 export default Events;
--- look_alive/src/components/Feed.js
+++ look_alive/src/components/Feed.js
@@ -1,19 +1,40 @@
+import React, { Component } from 'react';
 
-import { Route } from 'react-router-dom';
+import Events from './Events';
 
+class Feed extends Component {
 
-const Feed  = () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+    events: [],
+    };
+  }
 
-  return (
-    <div>
+  componentDidMount() {
+    fetch('https://api.airtable.com/v0/app2HujIdBEExdRd1/Events?api_key=keymcQ6E3LYsrFEc7')
+    .then((resp) => resp.json())
+      .then(data => {
+      console.log(data);
+      this.setState({ events: data.records });
+    }).catch(err => {
+      Error
+    });
+  }
 
-<Route path="/">
-
-      </Route>
-
-
-    </div>
-  )
-} 
+  render() {
+    return (
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col">
+            <div className="card-deck">
+              {this.state.events.map(event => <Events {...event.fields} /> )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Feed;
