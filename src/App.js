d439df5b670ed0a94a556b63ea07b271b1811784
Ricky Boyd <rickyboyd23@gmail.com>
2021-10-28 18:00:54 -0400

    bootstarp styling

--- src/App.js
+++ src/App.js
@@ -34,15 +34,17 @@ function App() {
     getPosts();
   }, [toggleFetch]);
 
-  return (
-    <div className="App">
+return (
+      <div className="App">
 
       <Navi />
 
-      <Route path="/" exact>
+    <Route path="/" exact>
+
       <div>
         <img src="http://theotherpress.ca/wp-content/uploads/2019/02/ARTS_Concert.jpg" class="img-fluid" alt="Responsive image">
-        </img>
+          </img>
+          
       </div>
 
       {posts.map((post) => (
@@ -53,45 +55,52 @@ function App() {
           setToggleFetch={setToggleFetch}
         />
       ))}
-      </Route>
+        
+    </Route>
 
-      <Route path="/newpost" exact>
+    <Route path="/newpost" exact>
+
       <div>
         <img src="https://i.pinimg.com/736x/56/db/23/56db23ae7ee02ec435492427a8e4f19e--funny-band-memes-marching-band-memes.jpg" class="img-fluid" alt="Responsive image">
         </img>
       </div>
 
-        <Form 
+      <Form 
           formType={'post'}
           toggleFetch={toggleFetch}
           setToggleFetch={setToggleFetch}
-        />
-      </Route>
+      />
 
-      <Route path="/edit/:post_id" exact>
-        <Form
+    </Route>
+
+    <Route path="/edit/:post_id" exact>
+
+      <Form
           formType={'put'}
           toggleFetch={toggleFetch}
           setToggleFetch={setToggleFetch}
-        />
-      </Route>
+      />
 
-      <Route path="/about">
+    </Route>
+
+    <Route path="/about">
       <div>
         <img src="https://www.washington.org/sites/default/files/styles/generic_hero_banner_1440_x_600/public/glass_animals_at_930_club_lisa_walker_credit.jpg?h=199d8c1f&itok=ksRkQb40" class="img-fluid" alt="Responsive image">
         </img>
-      </div>
-      <About/>
+        </div>
+        
+      <About />
+        
     </Route>
       
     <Route path="/events" exact>
-        <Events />
+      <Events />
     </Route>
       
 
       <Footer />
 
-    </div>
+      </div>
   );
 }
 
